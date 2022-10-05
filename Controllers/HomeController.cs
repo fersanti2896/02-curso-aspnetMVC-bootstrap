39fd91a480c95575aa181b058eaef3058964c45d
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-05 16:49:49 -0500

    Subiendo proyecto donde se vio una introduccion a Bootstrap

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using IntroduccionBootstrap.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IntroduccionBootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Vistas() { 
+            return View(); 
+        }
+
+        public IActionResult Alertas() {
+            return View();
+        }
+
+        public IActionResult Menus() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroduccionBootstrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
